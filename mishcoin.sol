cb7d3f8d107d1424f1b1e1c2dda8e7963d65ac7c
Mischa Spiegelmock <me@mish.dev>
2021-05-29 20:39:09 +0300

    initial import

---
+++ mishcoin.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: ABRMS
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract MishToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("MishCoin", "MISH") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 2;
+    }
+}
